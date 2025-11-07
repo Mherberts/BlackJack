25896ad3734c062cc1eab41f82a79b61e35fba75
Matheus Herberts <163153928+Mherberts@users.noreply.github.com>
2025-11-06 22:35:10 -0300

    Enhance game logic with tie handling and card purchase
    Added handling for tie scenario and card purchase logic.

--- Jogo.py
+++ Jogo.py
@@ -1,6 +1,6 @@
 import random
-from regras import blackjackbanca
-from auxiliares import compararbanca
+from regras import blackjack
+from auxiliares import compararbanca , compracartasjog
 
 strings = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" , "A"]
 cartas_jog = random.choices(strings, k=2)  
@@ -12,13 +12,34 @@ def iniciarjogo():
     print(cartas_banca[0])
     
     
-    if blackjackbanca(cartas_banca):
+    if blackjack(cartas_banca):
 
         print('Você perdeu, a banca tem um BlackJack!')
         print(cartas_banca)
 
+    elif blackjack(cartas_banca) and blackjack(cartas_jog):
+
+        print('Empate! A banca e você tem um Blackjack!')
+        print(cartas_banca)
+
     else:
-        pergunta = input("Você quer outra carta('hit'), parar('stop'), separar('split') ou dobrar('double')?").lower()
+
+        if cartas_jog[0] == cartas_jog[1]:
+            pergunta = input("Você quer outra carta('hit'), parar('stop'), separar('split') ou dobrar('double')?").lower()
         
-        if pergunta == 'stop':
-            compararbanca(cartas_banca, cartas_jog)
+            if pergunta == 'stop':
+                compararbanca(cartas_banca, cartas_jog)
+            
+            elif pergunta == 'hit':
+                compracartasjog(cartas_jog)
+                compararbanca(cartas_banca, cartas_jog)
+
+        else: 
+            pergunta = input("Você quer outra carta('hit'), parar('stop') ou dobrar('double')?").lower()
+        
+            if pergunta == 'stop':
+                compararbanca(cartas_banca, cartas_jog)
+
+            elif pergunta == 'hit':
+                compracartasjog(cartas_jog)
+                compararbanca(cartas_banca, cartas_jog)
