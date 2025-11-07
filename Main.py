5dda4caf42bae27c057f48d53b28c5006de45a2f
Matheus Herberts <163153928+Mherberts@users.noreply.github.com>
2025-11-06 22:34:49 -0300

    Add exit option to Blackjack game

--- Main.py
+++ Main.py
@@ -5,11 +5,13 @@ from Jogo import iniciarjogo
 print('===BLACKJACK===')
 
 while True:
-    inicio = input("Para começar, digite o que você deseja. Para regras, digite 'rules' , para começar o jogo digite 'start': ").lower()
+    inicio = input("Para começar, digite o que você deseja. Para regras, digite 'rules' , para começar o jogo digite 'start, para sair digite 'n'': ").lower()
     if inicio == "rules":
         printregras()
     elif inicio == "start":
         iniciarjogo()
+    elif inicio == 'n':
+        print('Obrigado por jogar! Espero que tenha sido divertido!!')
         break
     else:
         print("Comando inválido, tente novamente.")
